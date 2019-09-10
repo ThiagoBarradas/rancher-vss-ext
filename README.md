564f0bbe94a48a4e392b8b3e258f7e1a6775a4d5
Thiago Barradas <thiagobrowskt@gmail.com>
2019-09-10 20:15:22 -0300

    :art: add env vars by prefix and name

--- README.md
+++ README.md
@@ -31,9 +31,16 @@ For task `Rancher Get Current Version`:
 - **Wait Complete**: Wait upgrade and new services up
 - **Image**: Image slug name or full address. e.g. docker.azurecr.io/xxx/my-image
 - **Tag**: New tag. e.g. 'master-20190701.1', '1.5.3' or 'latest'
-- **Save result and use as cache**: Save current version in artifact and use as cache when call this task other time in the same pipeline
+- **Install Docker**: This task needs docker to work, if you haven't it installed, check this box. Works only with linux
+- **Environment Vars Prefix**: Import all env vars with this prefix to container
+- **Additional Environment Vars**: Import this env vars to container
 
 ## Release notes
+
+**New in 1.1.4**
+- Add env vars by prefix;
+- Add env vars by name;
+- Add option to install docker (only linux)
 
 **New in 1.0.18**
 - Fix get current version task output;
--- src/upgrade/upgrade.ts
+++ src/upgrade/upgrade.ts
@@ -3,16 +3,21 @@ import * as tl from 'azure-pipelines-task-lib/task';
 async function run() {
     try {
         // get inputs
-        var url: string = tl.getInput('url', true)
-        var accessKey: string = tl.getInput('accessKey', true)
-        var secretKey: string = tl.getInput('secretKey', true)
+        var installDocker: boolean = tl.getBoolInput('installDocker', false);
+        var url: string = tl.getInput('url', true);
+        var accessKey: string = tl.getInput('accessKey', true);
+        var secretKey: string = tl.getInput('secretKey', true);
         var force: boolean = tl.getBoolInput('force', false);
         var wait: boolean = tl.getBoolInput('wait', false);
         var image: string = tl.getInput('image', false);
         var tag: string = tl.getInput('tag', false);
-
+        var prefixVars: string = tl.getInput('prefixVars', false);
+        var manualVars: string = tl.getInput('manualVars', false);
+        
         // install tools
-        await tl.exec('sudo', ["apt", "install", "docker", "-y"]);
+        if (installDocker) {
+            await tl.exec('sudo', ["apt", "install", "docker", "-y"]);
+        }
         
         // mount command
         const args: Array<string> = new Array<string>();
@@ -23,7 +28,7 @@ async function run() {
         args.push('--url=' + url);
         args.push('--user=' + accessKey);
         args.push('--pass=' + secretKey);
-        
+
         if (force) {
             args.push("--force");
         } 
@@ -34,6 +39,30 @@ async function run() {
 
         if (image) {
             args.push("--image=" + image);
+        }
+
+        if (prefixVars || manualVars) {
+            args.push("--update-env");
+        }
+        
+        if (prefixVars) {
+            tl.getVariables().forEach(function (variable) {
+                if (variable.name.startsWith(prefixVars)) {
+                    args.push("--env=\"" + variable.name + "=" + variable.value + "\"");
+                }
+            });
+        }
+
+        if (manualVars) {
+            var vars = manualVars.replace("\r\n","\n").split("\n");
+            vars.forEach(function (varName) {
+                var value = tl.getVariable(varName);
+                if (value == null) {
+                    value = "";
+                }
+
+                args.push("--env=\"" + varName + "=" + value + "\"");
+            });
         }
 
         if (tag) {
